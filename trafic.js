1499a691eebed4900f39058241414786da9e5a5a
Abdul Kader <abdulkader@tikapp.link>
2024-09-15 11:12:51 +0200

    trafic useing if statment

--- trafic.js
+++ trafic.js
@@ -1,24 +1,26 @@
 "use strict";
-/**
- * The `state` property says what the traffic light's state (i.e. colour) is at
- * that moment.
- */
+
 const trafficLight = {
   state: "green",
 };
 
 let rotations = 0;
-
-  const currentState = trafficLight.state;
-
 while (rotations < 2) {
-  fo
+  const currentState = trafficLight.state;
   console.log("The traffic light is on", currentState);
 
+  if (currentState === "green"){
+    trafficLight.state = "orange";
+  } else if (currentState === "orange"){
+    trafficLight.state = "red";
+  } else if (currentState === "red"){
     rotations++;
-  
+    trafficLight.state = "green";
+  } else {
+    console.log("The traffic light stopped working.");
+    break;
+    }
   }
-}
 
   // TODO
   // if the color is green, turn it orange
